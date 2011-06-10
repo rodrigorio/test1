6cd2d3b870fcd78612e5315afd4e8d9fc53a3d01
matiasvelillamdq@gmail.com <matiasvelillamdq@gmail.com@ff6933cd-8362-641d-8811-dd1df8fd4985>
2011-06-10 22:13:48 +0000

    dggg

--- trunk/ rtesis --username rodrigo.a.rio@gmail.com/Tesis/index.php
+++ trunk/ rtesis --username rodrigo.a.rio@gmail.com/Tesis/index.php
@@ -6,7 +6,7 @@
 require_once 'includes/includePath.php';
 require_once 'includes/autoload.php';
 
-FrontController::getInstance()->throwExceptions(false) //poner en true cuando se testea, las excepciones se imprimen y cortan la ejecucion del sistema.
+FrontController::getInstance()->throwExceptions(true) //poner en true cuando se testea, las excepciones se imprimen y cortan la ejecucion del sistema.
                               ->setBaseUrl('/blogs1.0')
                               ->registerPlugin(new PluginSession())                       // routeStartup  preDispatch
                               ->registerPlugin(new PluginRedireccion404())                //               preDispatch   postDispatch
