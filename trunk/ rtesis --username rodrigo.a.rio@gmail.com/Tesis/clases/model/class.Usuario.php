ee993e19be50c080f0730e5290651e269e472e5a
matiasvelillamdq@gmail.com <matiasvelillamdq@gmail.com@ff6933cd-8362-641d-8811-dd1df8fd4985>
2011-06-11 04:50:42 +0000

    dggg

--- trunk/ rtesis --username rodrigo.a.rio@gmail.com/Tesis/clases/model/class.Usuario.php
+++ trunk/ rtesis --username rodrigo.a.rio@gmail.com/Tesis/clases/model/class.Usuario.php
@@ -12,6 +12,22 @@ class Usuario extends PersonaAbstract
     private $nombreUsuario;//de donde salio este atributo??
     private $contrasenia;
 
+    public function __construct(stdClass $oParams = null){
+        parent::__construct();
+
+        $vArray = get_object_vars($oParams);
+        $vThisVars = get_class_vars(__CLASS__);
+        if(is_array($vArray)){
+            foreach($vArray as $varName => $value){
+                if(array_key_exists($varName,$vThisVars)){
+                    $this->$varName = $value;
+                }else{
+                    throw new Exception("Unknown property $varName in "  . __CLASS__,-1);
+                }
+            }
+        }
+    }
+
     public function getNombreUsuario()
     {
         return $this->nombreUsuario;
