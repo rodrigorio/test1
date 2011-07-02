2856e2d08127fcb155e444c9ed38d337c41e1220
matiasvelillamdq <matiasvelillamdq@gmail.com>
2011-07-02 08:02:05 +0000

    fdsfss

---
+++ clases/pageControllers/comunidad/class.InvitacionesControllerComunidad.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @author Matias Velilla
+ */
+class InvitacionesControllerComunidad extends PageControllerAbstract
+{
+    /**
+     * Establece descripcion de invitaciones y el menu con 2 opciones,
+     * estado de invitaciones enviadas y formulario para enviar nueva invitacion
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Procesa el envio desde un formulario de invitacion.
+     */
+    public function procesar()
+    {
+    }
+
+    /**
+     * Vista para enviar una nueva invitacion
+     */
+    public function formulario()
+    {
+    }
+
+    /**
+     * Lista de todas las invitaciones realizadas y el estado en el que se encuentran
+     */
+    public function listado()
+    {
+    }
+}
