27384b3d3d3b3ac46d408fb21b0a29e4ddee5bef
profedelfino@gmail.com <profedelfino@gmail.com@ff6933cd-8362-641d-8811-dd1df8fd4985>
2013-04-17 23:56:24 +0000

    17 del 04 Andres creacion de intermediary y sql intermediary de entrevista preguntas

---
+++ trunk/ rtesis --username rodrigo.a.rio@gmail.com/Tesis/clases/persistencia/MYSQL/class.EntrevistaMySQLIntermediary.php
@@ -0,0 +1,23 @@
+<?php
+
+class EntrevistaMySQLIntermediary extends EntrevistaIntermediary
+{
+    private static $instance = null;
+
+    protected function __construct($conn) {
+        parent::__construct($conn);
+    }
+
+    /**
+     * Singleton
+     *
+     * @param mixed $conn
+     * @return GroupMySQLIntermediary
+     */
+    public static function &getInstance(IMYSQL $conn) {
+        if (null === self::$instance){
+            self::$instance = new self($conn);
+        }
+        return self::$instance;
+    }
+}
